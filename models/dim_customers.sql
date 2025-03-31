0671f00e812df429b63dc1c649e8e1adbea3a4c1
Sophiane Bouzelmat <sophiane.bouzelmat@gmail.com>
2025-03-31 12:48:21 +0000

    feef

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -6,15 +6,13 @@
 
 with customers as (
 
-    select *
-    from {{ref('stg_jaffle_shop__customers')}}
+    select * from {{ref('stg_jaffle_shop__customers')}}
 
 ),
 
 orders as (
 
-    select *
-    from {{ref('stg_jaffle_shop__orders')}}
+    select * from {{ref('stg_jaffle_shop__orders')}}
 
 ),
 
